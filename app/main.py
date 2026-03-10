9a64ceeede1fc27db26388e3dcb4616a0599e07c
RajashekarRM1 <rajashekar.maddi@rm1codershub.com>
2026-03-10 15:40:28 +0530

    first commit

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from routes import auth_routes, call_routes, conversation_routes, message_routes
+from routes import auth_routes, call_routes, conversation_routes, message_routes,converstion_particpant_route
 from websocket.chat_socket import manager
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=False,allow_methods=["*"],allow_headers=["*"],)
+
 
 app.include_router(auth_routes.router)
 app.include_router(conversation_routes.router)
 app.include_router(message_routes.router)
-app.include_router(conversation_routes.router)
+app.include_router(converstion_particpant_route.router)
 app.include_router(call_routes.router)
 
 
