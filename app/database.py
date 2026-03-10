e1cdfe87e5e0647d71f153f908605f78a35b9526
RajashekarRM1 <rajashekar.maddi@rm1codershub.com>
2026-03-10 16:16:32 +0530

    :affdsfsd

--- app/database.py
+++ app/database.py
@@ -2,34 +2,32 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
-# Load .env file
+# Load .env
 load_dotenv()
 
-# Read environment variables
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Encode password safely
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# Create engine
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
-# Create session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Base model
 Base = declarative_base()
 
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
