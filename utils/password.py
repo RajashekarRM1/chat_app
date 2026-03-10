32dc9180cd169e03f6cb944e5fb712e0ddd97396
RajashekarRM1 <rajashekar.maddi@rm1codershub.com>
2026-03-10 16:39:38 +0530

    :affdsfsd

--- utils/password.py
+++ utils/password.py
@@ -1,14 +1,9 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
+_password_context = CryptContext(schemes=["sha256_crypt"],deprecated="auto",)
 
-def hash_password(password: str):
-    password = password[:72]   # bcrypt limit
-    return pwd_context.hash(password)
+def hash_password(password: str) -> str:
+    return _password_context.hash(password)
 
-def verify_password(password: str, hashed_password: str):
-    password = password[:72]
-    return pwd_context.verify(password, hashed_password)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return _password_context.verify(plain_password, hashed_password)
